88293ddc739a1d8981c17733818680820e742e70
Ivan <hello.vaann@gmail.com>
2025-01-28 17:48:43 +0700

    hooks

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,34 +1,20 @@
-import { useState } from "react";
-const Counter = () => {
-    const [count, setCount] = useState(0);
-    // let count = 0;
 
-    const handleDecrement = ()=>{
-        alert("Decrement")
-        //count--;
-        setCount(count - 1);
-    }
-    const handleIncrement = ()=>{
-        alert("Increment")
-       // count++;
-         setCount(count + 1);
-        
-    }
-    // const handleReset = ()=>{
-    //     alert("Reset")
-    //     //count = 0;
-    //     setCount(0);
-    // }
+import useCounter from "../hooks/useCounter";
+
+
+
+const Counter = () => {
+    const {count, handleDecrement, handleIncrement, handleReset} = useCounter(); 
     return ( 
     <div style={{ display : "flex", gap : "16px ", }}>
         <button onClick={handleIncrement}>Tambah</button>
         <p>{count}</p>
         <button onClick={handleDecrement}>Kurang</button>
-        {/* <button onClick={handleReset}>Reset</button> */}
+        <button onClick={handleReset}>Reset</button>
     </div>
     
     )
    
-}
+};
 
 export default Counter;
---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const useCounter = () => {
+    const [count, setCount] = useState(0);
+
+const handleDecrement = ()=>{
+        alert("Decrement")
+        //count--;
+        setCount(count - 1);
+    }
+    const handleIncrement = ()=>{
+        alert("Increment")
+       // count++;
+         setCount(count + 1);
+        
+    }
+    const handleReset = ()=>{
+        alert("Reset")
+        //count = 0;
+        setCount(0);
+    } 
+    return {count, handleDecrement, handleIncrement, handleReset};
+};
+export default useCounter;
